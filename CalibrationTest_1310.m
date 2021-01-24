7ccaf0240bdb817e7928e29396e9d0813d042e2c
showtimeliu <junze.liu.9302@gmail.com>
2021-01-24 04:26:14 -0800

    working on FFT and mask

--- CalibrationTest_1310.m
+++ CalibrationTest_1310.m
@@ -1,11 +1,13 @@
 clearvars; close all; 
 
-strName = '20210119_TREAT_TestRun_NoPol_116058'; 
-strDir = 'D:\TREAT\20210119\'; 
+strName = '20210120_CoverslipOnRef_SampleBlk_117305'; 
+strDir = 'D:\Raw data\1310 System\20210120 Calibration\Ref arm only with coverslip\'; 
 strFile = sprintf('%s%s.dat', strDir, strName); 
 cellArrays = readHeader(strFile);
-nNumberLines = cellArrays{2,3}; % 1024
-nLineLength = cellArrays{2,4};  % 2096
+nNumberLines = cellArrays{2,3}; 
+nLineLength = cellArrays{2,4};  
+
+nMidLineLength = nLineLength / 2; 
 
 disp(strFile);
 [pdIMAQ, ~] = readDataFile(strFile, cellArrays); % the matrix pdIMAQ is a frame of multiple spectra: [LineLength, NumberOfLinesInAFrame]
@@ -14,4 +16,15 @@ disp(strFile);
 pdPara = pdIMAQ(:, :, 1); 
 pdPerp = pdIMAQ(:, :, 2); 
 
-keyboard; 
+pdData = pdPara; 
+
+%% calculate and apply mask 
+% find the peak to determine the boundaries of the mask 
+pdFFT = fft(pdData); 
+figure, plot(10 * log10((abs(pdFFT) .^ 2))); xlim([1, nMidLineLength]); 
+
+nLeft = 47; nRight = 71; 
+
+
+
+
